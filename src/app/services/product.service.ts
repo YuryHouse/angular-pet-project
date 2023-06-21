af2f03f01509076780590a8c689eb9966f7a4feb
YuryHouse <yury.house@yahoo.com>
2023-06-21 15:57:15 +0300

    feature: was created error handler

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,12 +2,16 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
 import {catchError, delay, Observable, throwError} from 'rxjs';
 import {IProduct} from '../models/product';
+import {ErrorService} from "./error.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private errorService: ErrorService
+    ) {
   }
 
   getAll(): Observable<IProduct[]> {
@@ -15,12 +19,12 @@ export class ProductService {
       params: new HttpParams().append('limit', 5)
     }).pipe(
       delay(2000),
-      catchError(this.errorHandler)
+      catchError(this.errorHandler.bind(this))
     )
   }
 
   private errorHandler(error: HttpErrorResponse) {
-
+    this.errorService.handle(error.message)
     return throwError(() => error.message)
   }
 
